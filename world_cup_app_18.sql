33e283baa200e7bff7019de31e99088f41bbfacf
Doggaz Jihed <jihed.doggaz@esprit.tn>
2018-03-30 17:20:51 +0100

    database

--- world_cup_app_18.sql
+++ world_cup_app_18.sql
@@ -11,7 +11,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 --
--- Database: `worldcuptest`
+-- Database: `world_cup_app_18`
 --
 
 -- --------------------------------------------------------
@@ -28,6 +28,29 @@ CREATE TABLE `ARTICLE` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
+
+--
+-- Structure de la table `player`
+--
+
+CREATE TABLE `PLAYER` (
+  `PLAYER_ID` int(4) NOT NULL,
+  `PLAYER_NAME` varchar(40) NOT NULL,
+  `PLAYER_AGE` int(2) NOT NULL,
+  `PLAYER_TEAM` varchar(40) DEFAULT NULL,
+  `PLAYER_POSITION` varchar(40) DEFAULT NULL,
+  `PLAYER_CLUB` varchar(25) NOT NULL,
+  `PLAYER_HEIGHT` varchar(25) NOT NULL,
+  `PLAYER_PICTURE` varchar(300) DEFAULT NULL,
+  `PLAYER_WEIGHT` varchar(25) NOT NULL,
+  `PLAYER_TSHIRT` int(2) NOT NULL,
+  `PLAYER_NOTE` int(3) NOT NULL,
+  `PLAYER_NUMBER_NOTE` int(3) NOT NULL,
+  `PLAYER_RATING` float NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
 
 --
 -- Table structure for table `fos_user`
@@ -165,6 +188,112 @@ CREATE TABLE `GOAL` (
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `team`
+--
+
+CREATE TABLE `TEAM` (
+  `TEAM_ID` int(3) NOT NULL,
+  `TEAM_NAME` varchar(45) DEFAULT NULL,
+  `TEAM_COACH` varchar(45) DEFAULT NULL,
+  `TEAM_INTMATCHPLAYED` int(1) DEFAULT '0',
+  `TEAM_INTMATCHWON` int(1) DEFAULT '0',
+  `TEAM_INTMATCHLOST` int(1) DEFAULT '0',
+  `TEAM_INTMATCHDRAW` int(1) DEFAULT '0',
+  `TEAM_GOALSFOR` int(2) DEFAULT '0',
+  `TEAM_GOALSAGAINST` int(2) DEFAULT '0',
+  `TEAM_POINTS` int(2) DEFAULT '0',
+  `TEAM_POSITION` int(3) DEFAULT '0',
+  `TEAM_GROUP` char(1) NOT NULL,
+  `TEAM_CONTINENT` varchar(45) NOT NULL,
+  `TEAM_LOGO` varchar(500) NOT NULL,
+  `TEAM_FLAG` varchar(500) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `team`
+--
+
+INSERT INTO `team` (`TEAM_ID`, `TEAM_NAME`, `TEAM_COACH`, `TEAM_INTMATCHPLAYED`, `TEAM_INTMATCHWON`, `TEAM_INTMATCHLOST`, `TEAM_INTMATCHDRAW`, `TEAM_GOALSFOR`, `TEAM_GOALSAGAINST`, `TEAM_POINTS`, `TEAM_POSITION`, `TEAM_GROUP`, `TEAM_CONTINENT`, `TEAM_LOGO`, `TEAM_FLAG`) VALUES
+(28, 'Brazil', 'Tite', 0, 0, 0, 0, 0, 0, 0, 0, 'E', 'Amerique de sud', 'téléchargement.png', '225px-Flag_of_Brazil.svg.png'),
+(25, 'Tunisie', 'Nabil Maaloul', 0, 0, 0, 0, 0, 0, 0, 0, 'G', 'Afrique', 'file:/C:/Users/pacha/Pictures/logos/Tunisia_FA.svg.png', 'file:/C:/Users/pacha/Pictures/drapeaux/tunisia.png'),
+(24, 'Belgique', 'Roberto Martínez Montoliú', 0, 0, 0, 0, 0, 0, 0, 0, 'G', 'Europe', 'file:/C:/Users/pacha/Pictures/logos/URBSFA.jpg', 'file:/C:/Users/pacha/Pictures/drapeaux/drapeau-belgique.jpg'),
+(29, 'France', 'Didier Deschamps', 0, 0, 0, 0, 0, 0, 0, 0, 'C', 'Europe', 'file:/C:/Users/pacha/Pictures/logos/00552682.jpg', 'file:/C:/Users/pacha/Pictures/drapeaux/225px-Flag_of_France.svg.png'),
+(30, 'Egypt', 'Hector Cuper', 0, 0, 0, 0, 0, 0, 0, 0, 'A', 'Afrique', 'file:/C:/Users/pacha/Pictures/logos/Football_Égypte_federation.svg.png', 'file:/C:/Users/pacha/Pictures/drapeaux/255px-Flag_of_Egypt.svg.png'),
+(31, 'Angleterre', 'Gareth Southgate', 0, 0, 0, 0, 0, 0, 0, 0, 'G', 'Europe', 'file:/C:/Users/pacha/Pictures/logos/200px-Football_Angleterre_federation.png', 'file:/C:/Users/pacha/Pictures/drapeaux/225px-Flag_of_England.svg.png'),
+(32, 'Panama', 'Hernán Darío Gómez', 0, 0, 0, 0, 0, 0, 0, 0, 'G', 'Amerique de nord', 'file:/C:/Users/pacha/Pictures/logos/téléchargement%20(1).png', 'file:/C:/Users/pacha/Pictures/drapeaux/225px-Flag_of_Panama.svg.png'),
+(33, 'Russie', 'Stanislav Tchertchessov', 0, 0, 0, 0, 0, 0, 0, 0, 'A', 'Europe', 'file:/C:/Users/pacha/Pictures/logos/téléchargement%20(2).png', 'file:/C:/Users/pacha/Pictures/drapeaux/81NfOcqiXtL._SX355_.jpg'),
+(39, 'Arabie Saoudite', 'Juan Antonio Pizzi', 0, 0, 0, 0, 0, 0, 0, 0, 'A', 'Asie & Océanie', 'file:/C:/Users/pacha/Pictures/logos/saudi_arabia_national_football.png', 'file:/C:/Users/pacha/Pictures/drapeaux/téléchargement%20(5).png'),
+(38, 'Uruguay', 'Óscar Tabárez', 0, 0, 0, 0, 0, 0, 0, 0, 'A', 'Amerique de sud', 'file:/C:/Users/pacha/Pictures/téléchargement%20(4).png', 'file:/C:/Users/pacha/Pictures/téléchargement%20(3).png'),
+(40, 'Espagne', 'Julen Lopetegui', 0, 0, 0, 0, 0, 0, 0, 0, 'B', 'Europe', 'file:/C:/Users/pacha/Pictures/logos/téléchargement%20(7).png', 'file:/C:/Users/pacha/Pictures/drapeaux/téléchargement%20(6).png'),
+(41, 'Portugal', 'Fernando Santos', 0, 0, 0, 0, 0, 0, 0, 0, 'B', 'Europe', 'file:/C:/Users/pacha/Pictures/Football_Portugal_federation.svg.png', 'file:/C:/Users/pacha/Pictures/pt.png'),
+(42, 'Maroc', 'Hervé Renard', 0, 0, 0, 0, 0, 0, 0, 0, 'B', 'Afrique', 'file:/C:/Users/pacha/Pictures/20170607084347!Football_Maroc_federation.png', 'file:/C:/Users/pacha/Pictures/225px-Flag_of_Morocco.svg.png'),
+(43, 'Iran', 'Carlos Queiroz', 0, 0, 0, 0, 0, 0, 0, 0, 'B', 'Asie & Océanie', 'file:/C:/Users/pacha/Pictures/Football_Federation_Islamic_Republic_of_Iran.png', 'file:/C:/Users/pacha/Pictures/225px-Flag_of_Iran.svg.png'),
+(44, 'Australie', 'Bert van Marwijk', 0, 0, 0, 0, 0, 0, 0, 0, 'C', 'Asie & Océanie', 'file:/C:/Users/pacha/Pictures/téléchargement.jpg', 'file:/C:/Users/pacha/Pictures/au.png'),
+(45, 'Peru', 'Ricardo Gareca', 0, 0, 0, 0, 0, 0, 0, 0, 'C', 'Amerique de sud', 'file:/C:/Users/pacha/Pictures/1200px-Fpf-logo.svg.png', 'file:/C:/Users/pacha/Pictures/téléchargement%20(8).png'),
+(46, 'Danemark', 'Åge Hareide', 0, 0, 0, 0, 0, 0, 0, 0, 'C', 'Europe', 'file:/C:/Users/pacha/Pictures/téléchargement%20(10).png', 'file:/C:/Users/pacha/Pictures/téléchargement%20(9).png'),
+(47, 'Argentine', 'Jorge Sampaoli', 0, 0, 0, 0, 0, 0, 0, 0, 'D', 'Amerique de sud', 'file:/C:/Users/pacha/Pictures/téléchargement%20(11).png', 'file:/C:/Users/pacha/Pictures/225px-Flag_of_Argentina.svg.png'),
+(48, 'Nigeria', 'Salisu Yusuf', 0, 0, 0, 0, 0, 0, 0, 0, 'D', 'Afrique', 'file:/C:/Users/pacha/Pictures/téléchargement%20(1).jpg', 'file:/C:/Users/pacha/Pictures/1200px-Flag_of_Nigeria.svg.png'),
+(49, 'Islande', 'Heimir Hallgrímsson', 0, 0, 0, 0, 0, 0, 0, 0, 'D', 'Europe', '1200px-Fpf-logo.svg.png', '1200px-Fpf-logo.svg.png'),
+(50, 'Croatie', 'Ante ?a?i?, Zlatko Dali?', 0, 0, 0, 0, 0, 0, 0, 0, 'D', 'Europe', 'file:/C:/Users/pacha/Pictures/téléchargement%20(13).png', 'file:/C:/Users/pacha/Pictures/Civil_Ensign_of_Croatia.svg.png'),
+(51, 'Suisse', 'Vladimir Petkovi?', 0, 0, 0, 0, 0, 0, 0, 0, 'E', 'Europe', 'file:/C:/Users/pacha/Pictures/téléchargement%20(14).png', 'file:/C:/Users/pacha/Pictures/1200px-Flag_of_Switzerland.svg.png'),
+(52, 'Costa Rica', 'Oscar Ramírez', 0, 0, 0, 0, 0, 0, 0, 0, 'E', 'Amerique de nord', 'file:/C:/Users/pacha/Pictures/téléchargement%20(15).png', 'file:/C:/Users/pacha/Pictures/225px-Flag_of_Costa_Rica_(state).svg.png'),
+(53, 'Serbie', 'Mladen Krstaji?', 0, 0, 0, 0, 0, 0, 0, 0, 'E', 'Europe', 'file:/C:/Users/pacha/Pictures/téléchargement%20(16).png', 'file:/C:/Users/pacha/Pictures/225px-Flag_of_Serbia.svg.png'),
+(54, 'Allemagne', 'Joachim Löw', 0, 0, 0, 0, 0, 0, 0, 0, 'F', 'Europe', '00552682.jpg', '225px-Flag_of_Argentina.svg.png'),
+(69, 'ajouter', 'aziz', 0, 0, 0, 0, 0, 0, 0, 0, 'B', 'Asie & Océanie', '1200px-Fpf-logo.svg.png', '81NfOcqiXtL._SX355_.jpg'),
+(68, 'ttt', 'tt', 0, 0, 0, 0, 0, 0, 0, 0, 'A', 'Asie & Océanie', '200px-Football_Angleterre_federation.png', '1200px-Fpf-logo.svg.png'),
+(67, 'aaa', 'aaaa', 0, 0, 0, 0, 0, 0, 0, 0, 'A', 'Afrique', '1200px-Fpf-logo.svg.png', '1200px-Fpf-logo.svg.png');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `stadium`
+--
+
+CREATE TABLE `STADIUM` (
+  `STADIUM_ID` int(1) NOT NULL,
+  `STADIUM_NAME` varchar(25) NOT NULL,
+  `STADIUM_LOCATION` varchar(50) NOT NULL,
+  `STADIUM_CAPACITY` int(1) NOT NULL,
+  `STADIUM_PICTURE` varchar(500) DEFAULT NULL,
+  `STADIUM_LOCATION_X` varchar(50) NOT NULL,
+  `STADIUM_LOCATION_Y` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `event`
+--
+
+CREATE TABLE `EVENT` (
+  `EVENT_ID` int(1) NOT NULL,
+  `EVENT_NAME` varchar(25) NOT NULL,
+  `EVENT_DATE` date NOT NULL,
+  `EVENT_END` date NOT NULL,
+  `EVENT_GOUV` varchar(50) DEFAULT NULL,
+  `EVENT_PLACE` varchar(50) NOT NULL,
+  `EVENT_DESC` varchar(200) NOT NULL,
+  `NBR_PARTICIP` int(4) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `station`
+--
+
+CREATE TABLE `STATION` (
+  `STATION_ID` int(11) NOT NULL,
+  `STATION_NAME` varchar(20) NOT NULL,
+  `STATION_LOCATION` varchar(30) NOT NULL,
+  `STATION_TYPE` varchar(30) NOT NULL,
+  `STATION_LOCATION_X` varchar(50) NOT NULL,
+  `STATION_LOCATION_Y` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `HOTEL`
 --
 
@@ -203,29 +332,9 @@ CREATE TABLE `PARTICIPATION` (
 
 -- --------------------------------------------------------
 
---
--- Table structure for table `USER`
---
 
-CREATE TABLE `USER` (
-  `USER_ID` int(11) NOT NULL,
-  `USER_NAME` varchar(30) NOT NULL,
-  `USER_LASTNAME` varchar(30) NOT NULL,
-  `USER_BIRTHDAY` date NOT NULL,
-  `USER_EMAIL` varchar(30) NOT NULL,
-  `USER_LOGIN` varchar(30) NOT NULL,
-  `USER_PASSWORD` varchar(30) NOT NULL,
-  `USER_CIN` varchar(30) DEFAULT NULL,
-  `USER_TYPE` varchar(45) NOT NULL DEFAULT 'membre'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `USER`
---
 
-INSERT INTO `USER` (`USER_ID`, `USER_NAME`, `USER_LASTNAME`, `USER_BIRTHDAY`, `USER_EMAIL`, `USER_LOGIN`, `USER_PASSWORD`, `USER_CIN`, `USER_TYPE`) VALUES
-(4, 'sdf', 'hjgf', '2018-03-28', 'jhg', 'fjhg', 'fjhg', 'jhgf', 'membre'),
-(5, 'kjhg', 'jkh', '2018-03-08', 'hg', 'kjhg', 'jkhg', 'gkj', 'membre');
 
 --
 -- Indexes for dumped tables
@@ -251,12 +360,58 @@ ALTER TABLE `NOTE`
 --
 ALTER TABLE `PARTICIPATION`
   ADD PRIMARY KEY (`PARTICIPATION_ID`);
+  
+  --
+-- Index pour la table `hotel`
+--
+ALTER TABLE `HOTEL`
+  ADD PRIMARY KEY (`HOTEL_ID`);
 
 --
--- Indexes for table `USER`
+-- Index pour la table `player`
 --
-ALTER TABLE `USER`
-  ADD PRIMARY KEY (`USER_ID`);
+ALTER TABLE `PLAYER`
+  ADD PRIMARY KEY (`PLAYER_ID`),
+  ADD KEY `FK_PLAYER_TEAM` (`PLAYER_TEAM`);
+
+--
+-- Index pour la table `stadium`
+--
+ALTER TABLE `STADIUM`
+  ADD PRIMARY KEY (`STADIUM_ID`);
+  
+--
+-- Index pour la table `article`
+--
+ALTER TABLE `ARTICLE`
+  ADD PRIMARY KEY (`ARTICLE_ID`);
+  
+--
+-- Index pour la table `goal`
+--
+ALTER TABLE `GOAL`
+  ADD PRIMARY KEY (`GOAL_ID`);
+
+
+--
+-- Index pour la table `game`
+--
+ALTER TABLE `GAME`
+  ADD PRIMARY KEY (`GAME_ID`);
+
+--
+-- Index pour la table `station`
+--
+ALTER TABLE `STATION`
+  ADD PRIMARY KEY (`STATION_ID`);
+
+--
+-- Index pour la table `team`
+--
+ALTER TABLE `TEAM`
+  ADD PRIMARY KEY (`TEAM_ID`);
+
+
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -267,6 +422,54 @@ ALTER TABLE `USER`
 --
 ALTER TABLE `fos_user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  
+--
+-- AUTO_INCREMENT pour la table `player`
+--
+ALTER TABLE `PLAYER`
+  MODIFY `PLAYER_ID` int(4) NOT NULL AUTO_INCREMENT;
+  
+--
+-- AUTO_INCREMENT pour la table `article`
+--
+ALTER TABLE `ARTICLE`
+  MODIFY `ARTICLE_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  
+--
+-- AUTO_INCREMENT pour la table `game`
+--
+ALTER TABLE `GAME`
+  MODIFY `GAME_ID` int(3) NOT NULL AUTO_INCREMENT;
+  
+--
+-- AUTO_INCREMENT pour la table `goal`
+--
+ALTER TABLE `GOAL`
+  MODIFY `GOAL_ID` int(3) NOT NULL AUTO_INCREMENT;
+  
+--
+-- AUTO_INCREMENT pour la table `hotel`
+--
+ALTER TABLE `HOTEL`
+  MODIFY `HOTEL_ID` int(3) NOT NULL AUTO_INCREMENT;
+  
+--
+-- AUTO_INCREMENT pour la table `stadium`
+--
+ALTER TABLE `STADIUM`
+  MODIFY `STADIUM_ID` int(1) NOT NULL AUTO_INCREMENT;
+  
+--
+-- AUTO_INCREMENT pour la table `station`
+--
+ALTER TABLE `STATION`
+  MODIFY `STATION_ID` int(11) NOT NULL AUTO_INCREMENT;
+  
+--
+-- AUTO_INCREMENT pour la table `team`
+--
+ALTER TABLE `TEAM`
+  MODIFY `TEAM_ID` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT for table `NOTE`
@@ -280,8 +483,4 @@ ALTER TABLE `NOTE`
 ALTER TABLE `PARTICIPATION`
   MODIFY `PARTICIPATION_ID` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `USER`
---
-ALTER TABLE `USER`
-  MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
